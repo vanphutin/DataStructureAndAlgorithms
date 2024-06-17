2d363fd76433a6c0ca234b161286c8725edc1cff
vanphutin2004 <vanphutin.contact@gmail.com>
2024-06-17 16:40:43 +0700

    tim kiem tuyen tinh

--- study/5.SearchAndSort/LinearSearch.cpp
+++ study/5.SearchAndSort/LinearSearch.cpp
@@ -1,23 +1,46 @@
 #include <iostream>
 #include <vector>
 
-int linearSearch(const std::vector<int>& arr, int key) {
-    for (int i = 0; i < arr.size(); ++i) {
-        if (arr[i] == key) {
-            return i;
-        }
+using namespace std;
+//O(n)
+
+// ham nhap mang
+void nhap(int a[], int n) {
+    cout << "Nhap cac phan tu cua mang:\n";
+    for(int i = 0; i < n; i++) {
+        cout << "Nhap phan tu thu " << i << ": ";
+        cin >> a[i];
     }
-    return -1; // N?u không tìm th?y
 }
 
+//ham xuat mang
+void xuat(int a[],int n){
+	cout <<"\nCac phan tu trong mang la: ";
+	for(int i = 0 ; i< n ; i++){
+		cout<<a[i]<<" ";
+	}
+}
+//ham tim kiem : tim kiem tuyen tinh
+int linearSearch(int a[], int n, int key) {
+    for(int i = 0 ; i <= n ; i++){
+    	if(a[i] == key){
+    		return i;
+		}
+	}
+	return -1;
+}
 int main() {
-    std::vector<int> arr = {4, 2, 7, 1, 9, 3};
-    int key = 7;
-    int index = linearSearch(arr, key);
+    int a[1001], n;
+    cout << "Nhap so luong phan tu trong mang (> 0): ";
+    cin >> n;
+    nhap(a,n);
+    xuat(a,n);
+    int key ; cout <<"\nNhap gia tri can tim : "; cin>>key;
+    int index = linearSearch(a, n, key);
     if (index != -1) {
-        std::cout << "Element found at index " << index << std::endl;
+        std::cout << "Vi tri can tim la: " << index << std::endl;
     } else {
-        std::cout << "Element not found" << std::endl;
+        std::cout << "Khong co vi tri nao !!" << std::endl;
     }
     return 0;
 }
