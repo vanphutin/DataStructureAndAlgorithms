5f0e692af62fbd2bf4f82b6697ad5f15ac1777af
vanphutin2004 <vanphutin.contact@gmail.com>
2024-09-14 00:25:31 +0700

    sort |insertion sort

---
+++ src/5.SearchAndSort/sort/insertion_sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void nhap_mang(int a[], int &n){
+    cout<<"Nhap sl p.tu: "; 
+    cin>>n;
+    for(int i = 0 ; i < n ; i++){
+        cin>>a[i];
+    }
+}
+
+void xuat_mang(int a[], int n){
+    cout<<"Xuat mang:\n";
+    for(int i = 0 ; i < n ; i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+void insertion_sort(int a[],int n){
+	int i,j ;
+	for(i = 0 ; i < n ; i++){
+		for(j = i + 1 ; j < n ; j++){
+			if(a[j] < a[i]){
+				swap(a[i],a[j]);
+			}
+		}
+	}
+	xuat_mang(a,n);
+
+}
+
+int main(){
+    int a[100001], n;
+    nhap_mang(a, n);
+    insertion_sort(a,n);
+    return 0;
+}
+
